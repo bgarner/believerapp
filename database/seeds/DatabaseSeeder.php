d95b2e10115ff5b8ddfd16273587f4f006622712
Brent Garner <bgarner@gmail.com>
2019-01-16 14:27:00 -0700

    adding the brand model, adding the brand seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Eloquent::unguard();
+        // Disable Foreign key check for this connection before running seeders
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
         // $this->call(UsersTableSeeder::class);
+        $this->call(BrandsTableSeeder::class);
     }
 }
