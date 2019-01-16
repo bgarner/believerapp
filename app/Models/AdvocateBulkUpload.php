84efe9d6318ef66ba9405bb89ca7660b0e1f56d3
Brent Garner <bgarner@gmail.com>
2019-01-16 14:44:48 -0700

    advocate bulk upload model and seeder

---
+++ app/Models/AdvocateBulkUpload.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class AdvocateBulkUpload extends Model
+{
+    use SoftDeletes;
+    protected $table = 'advocate_bulk_uploads';
+    protected $fillable = [
+        'first', 'last', 'email', 'registered', 'level', 'group_segmentation', 'user_id_uploader'
+    ];
+}
