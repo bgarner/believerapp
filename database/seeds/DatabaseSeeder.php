b39bb31f964ff6c3c9174bc83170c0d3f1001a4a
Brent Garner <bgarner@gmail.com>
2019-01-17 21:40:48 -0700

    adding the advocate profile model

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,5 +22,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(ChallengeTypesSeeder::class);
         $this->call(RewardsTypesTableSeeder::class);
+        $this->call(AdvocateGroupsSeeder::class);
     }
 }
