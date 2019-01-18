4d1e4c5139be0bd3ebd3e24bbef1357de23ec9f7
Brent Garner <bgarner@gmail.com>
2019-01-17 21:00:10 -0700

    user table seeder - take 1

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(BrandsTableSeeder::class);
         $this->call(AdvocateBulkUploadsSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
