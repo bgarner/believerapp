feb5e2d1a3ee611977d05442667db42d93d231cb
Brent Garner <bgarner@gmail.com>
2019-01-18 20:57:23 -0700

    adding the challenge model

---
+++ app/Models/Challenge.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Challenge extends Model
+{
+    use SoftDeletes;
+    protected $table = 'challenges';
+    protected $fillable = [
+        'name', 'content', 'start', 'end', 'brand_id', 'created_by', 'is_draft', 'points', 'challenge_type'
+    ];
+}
