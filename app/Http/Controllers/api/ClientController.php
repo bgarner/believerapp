697ed88ed2ac9c2a34699dd9cac6496c1db9eb5b
Brent Garner <bgarner@gmail.com>
2019-02-03 21:13:29 -0700

    merging...

--- app/Http/Controllers/api/ClientController.php
+++ app/Http/Controllers/api/ClientController.php
@@ -1,19 +1,9 @@
 <?php
-<<<<<<< HEAD
-
 namespace App\Http\Controllers\api;
-
-=======
-namespace App\Http\Controllers\api;
->>>>>>> 2217a77ddd700316a2f10733a19bf420b369f987
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\AdvocateProfile;
 use App\Models\Brand;
-<<<<<<< HEAD
-
-=======
->>>>>>> 2217a77ddd700316a2f10733a19bf420b369f987
 class ClientController extends Controller
 {
     /**
@@ -24,18 +14,11 @@ class ClientController extends Controller
     public function index(Request $request)
     {
         //find the user's location
-<<<<<<< HEAD
-        
-=======
->>>>>>> 2217a77ddd700316a2f10733a19bf420b369f987
         //find clients who are close to that location
         $clients = Brand::all();
         //return the list of clients
         return $clients;
     }
-<<<<<<< HEAD
-}
-=======
 
     public function show(Request $request)
     {
@@ -62,4 +45,3 @@ class ClientController extends Controller
 
     }
 }
->>>>>>> 2217a77ddd700316a2f10733a19bf420b369f987
