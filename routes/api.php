939910455c6edf7335bc78a9118c74212151ada5
Brent Garner <bgarner@gmail.com>
2019-02-03 21:05:35 -0700

    scafoling out the controllers for the api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,23 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/v1/clients', 'api\ClientController@index');
+Route::get('/v1/clients/{id}', 'api\ClientController@show');
+Route::post('/v1/clients/{id}/follow', 'api\ClientController@follow');
+Route::post('/v1/clients/{id}/refer', 'api\ClientController@refer');
+Route::post('/v1/clients/{id}/share', 'api\ClientController@share');
+
+Route::get('/v1/missions', 'api\MissionController@index');
+Route::get('/v1/missions/{id}', 'api\MissionController@show');
+Route::get('/v1/missions/client/{id}', 'api\MissionController@showByClient');
+Route::get('/v1/missions/{id}/accept', 'api\MissionController@accept');
+Route::get('/v1/missions/{id}/complete', 'api\MissionController@complete');
+
+Route::get('/v1/rewards', 'api\RewardController@index');
+Route::get('/v1/rewards/{id}', 'api\RewardController@show');
+Route::get('/v1/rewards/{id}/redeem', 'api\RewardController@redeem');
+
+Route::get('/v1/profile', 'api\ProfileController@index');
+Route::get('/v1/profile/pointbalance', 'api\ProfileController@balance');
+Route::get('/v1/profile/edit', 'api\ProfileController@edit');
