d11e2c77bc3450adc115bce6b4922fb0d0eca2a8
Brent Garner <bgarner@gmail.com>
2019-02-04 21:19:49 -0700

    find brands close to user

--- app/Http/Controllers/api/ClientController.php
+++ app/Http/Controllers/api/ClientController.php
@@ -1,9 +1,12 @@
 <?php
 namespace App\Http\Controllers\api;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\AdvocateProfile;
+use App\User;
 use App\Models\Brand;
+
 class ClientController extends Controller
 {
     /**
@@ -13,21 +16,15 @@ class ClientController extends Controller
      */
     public function index(Request $request)
     {
-        //find the user's location
-        //find clients who are close to that location
-        $clients = Brand::all();
-        //return the list of clients
-        return $clients;
+        //match the first char of the postal code to brands with the same first char
+        $user = User::find($request->user_id);
+        $user_postal_code_fragment = substr($user->postal_code, 0, 1) . '%';
+        return Brand::where('postal_code','like',$user_postal_code_fragment)->get();        
     }
 
     public function show(Request $request)
     {
         return Brand::find($request->id);
-    }
-
-    public function showByClient(Request $request)
-    {
-
     }
 
     public function follow(Request $request)
