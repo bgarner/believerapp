a1aea66f1e0a539801aa28ff74c8ad9d23b70f80
Brent Garner <bgarner@gmail.com>
2019-02-04 21:46:49 -0700

    follower function

--- app/Http/Controllers/api/ClientController.php
+++ app/Http/Controllers/api/ClientController.php
@@ -6,17 +6,18 @@ use App\Http\Controllers\Controller;
 use App\Models\AdvocateProfile;
 use App\User;
 use App\Models\Brand;
+use App\Models\Follower;
 
 class ClientController extends Controller
 {
-    /**
-     * Returns a list of clients available to follow, 
-     * ordered by who is closest to the users 
-     * location (post code, city, etc), as per their profile
-     */
+
     public function index(Request $request)
     {
         //match the first char of the postal code to brands with the same first char
+        // GET http://localhost:8000/api/v1/clients
+        // {
+        //     "user_id": 123   
+        // }
         $user = User::find($request->user_id);
         $user_postal_code_fragment = substr($user->postal_code, 0, 1) . '%';
         return Brand::where('postal_code','like',$user_postal_code_fragment)->get();        
@@ -24,12 +25,31 @@ class ClientController extends Controller
 
     public function show(Request $request)
     {
+        //GET http://localhost:8000/api/v1/clients/1
+        // {
+        //     "id": 1,
+        // }
         return Brand::find($request->id);
     }
 
     public function follow(Request $request)
     {
-
+        // POST http://localhost:8000/api/v1/clients/follow
+        // {
+        //     "user": 32,
+        //     "client": 11
+        // }
+        $checkFollow = Follower::where('user_id','=',$request->user)
+                                ->where('brand_id','=',$request->client)
+                                ->get();
+        
+        if(count($checkFollow)) {
+            return 'already followed';
+        } else {
+            $follow = new Follower(['brand_id' => $request->client, 'user_id' => $request->user]);
+            $follow->save();
+            return $follow->updated_at;
+        }
     }
 
     public function refer(Request $request)
