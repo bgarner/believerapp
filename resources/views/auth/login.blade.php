80a33701a3ce7b08a3c78db675ca6fb8b73503ff
Brent Garner <bgarner@gmail.com>
2019-02-12 20:46:26 -0700

    adding the logos, working on sign in page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
 
   <!-- General CSS Files -->
   <link rel="stylesheet" href="/css/app.css">
-  
+
 </head>
 
 <body>
@@ -17,7 +17,7 @@
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+              <img src="/images/logo-light.png" alt="logo" width="200" class="">
             </div>
 
             <div class="card card-primary">
@@ -61,29 +61,12 @@
                     </button>
                   </div>
                 </form>
-                <div class="text-center mt-4 mb-3">
-                  <div class="text-job text-muted">Login With Social</div>
-                </div>
-                <div class="row sm-gutters">
-                  <div class="col-6">
-                    <a class="btn btn-block btn-social btn-facebook">
-                      <span class="fab fa-facebook"></span> Facebook
-                    </a>
-                  </div>
-                  <div class="col-6">
-                    <a class="btn btn-block btn-social btn-twitter">
-                      <span class="fab fa-twitter"></span> Twitter
-                    </a>                                
-                  </div>
-                </div>
-
-              </div>
-            </div>
+  
             <div class="mt-5 text-muted text-center">
               Don't have an account? <a href="auth-register.html">Create One</a>
             </div>
             <div class="simple-footer">
-              Copyright &copy; Stisla 2018
+              Copyright &copy; Believer <?=date('Y')?>
             </div>
           </div>
         </div>
