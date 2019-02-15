e842a74e1263516bd888bbc8a7a492bf458f1437
Brent Garner <bgarner@gmail.com>
2019-02-14 19:40:00 -0700

    cleaning up the admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,11 +1,8 @@
 <?php
- 
 
-//CLient Routes
-Route::resource('/admin/clients', '\Admin\Client\ClientController');
+//Route::resource('/admin', 'Admin\AdminDashboardController');
+Route::get('/admin', array('uses' => 'Admin\AdminDashboardController@index'));
 
-//Route::get('/admin/clients', array('uses'=> 'Admin\ClientController@index'));
-
-Route::get('/admin/rewards', array('uses'=> 'Admin\RewardController@index'));
-
-Route::get('/admin/reports', array('uses'=> 'Admin\ReportController@index'));
+Route::resource('/admin/clients', 'Admin\ClientController');
+Route::resource('/admin/rewards', 'Admin\RewardController');
+Route::resource('/admin/reports', 'Admin\ReportController');
