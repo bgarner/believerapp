969fde00acdf25073b694c44ffae2c2e1ef6a851
Brent Garner <bgarner@gmail.com>
2019-03-12 21:29:05 -0600

    home page stuff

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -10,10 +10,10 @@ class HomeController extends Controller
     public function __construct()
     {
       //  $this->middleware('auth');
-    }  
+    }
 
     public function index()
     {
-        return view('test');
+        return view('web.index');
     }
 }
