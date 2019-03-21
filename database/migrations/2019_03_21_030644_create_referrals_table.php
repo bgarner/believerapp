092f4d3a459dd401259b3de9f33b68c7397c5ae4
Brent Garner <bgarner@gmail.com>
2019-03-20 21:17:47 -0600

    migration for referrals

--- database/migrations/2019_03_21_030644_create_referrals_table.php
+++ database/migrations/2019_03_21_030644_create_referrals_table.php
@@ -15,6 +15,12 @@ class CreateReferralsTable extends Migration
     {
         Schema::create('referrals', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('brand_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->integer('referred_by_id');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
