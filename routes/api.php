16890b20b6827ebeaac4faef2abdc1bdb12449e0
Brent Garner <bgarner@gmail.com>
2019-03-24 22:48:47 -0600

    uppercase, lowercase...

--- routes/api.php
+++ routes/api.php
@@ -21,31 +21,31 @@ Route::post('user/login', 'API\LoginController@login');
 //     return auth()->user();
 // });
 
-Route::post('/v1/clients', 'api\ClientController@index');
-Route::post('/v1/clientsFollowedByUser', 'api\ClientController@clientsFollowedByUser');
-Route::post('/v1/clients/show', 'api\ClientController@show');
-Route::post('/v1/clients/follow', 'api\ClientController@follow');
-Route::post('/v1/clients/unfollow', 'api\ClientController@unfollow');
-Route::post('/v1/clients/refer', 'api\ClientController@refer');
-Route::post('/v1/clients/share', 'api\ClientController@share');
+Route::post('/v1/clients', 'API\ClientController@index');
+Route::post('/v1/clientsFollowedByUser', 'API\ClientController@clientsFollowedByUser');
+Route::post('/v1/clients/show', 'API\ClientController@show');
+Route::post('/v1/clients/follow', 'API\ClientController@follow');
+Route::post('/v1/clients/unfollow', 'API\ClientController@unfollow');
+Route::post('/v1/clients/refer', 'API\ClientController@refer');
+Route::post('/v1/clients/share', 'API\ClientController@share');
 
-Route::post('/v1/missions', 'api\MissionController@index');
-Route::post('/v1/missions/show', 'api\MissionController@show');
-Route::post('/v1/missions/client', 'api\MissionController@showByClient');
-Route::post('/v1/missions/accept', 'api\MissionController@accept');
-Route::post('/v1/missions/complete', 'api\MissionController@complete');
+Route::post('/v1/missions', 'API\MissionController@index');
+Route::post('/v1/missions/show', 'API\MissionController@show');
+Route::post('/v1/missions/client', 'API\MissionController@showByClient');
+Route::post('/v1/missions/accept', 'API\MissionController@accept');
+Route::post('/v1/missions/complete', 'API\MissionController@complete');
 
-Route::post('/v1/profile', 'api\ProfileController@show');
-Route::post('/v1/profile/history', 'api\ProfileController@challengeHistory');
-Route::post('/v1/profile/pointbalance/', 'api\ProfileController@balance');
-Route::post('/v1/profile/edit', 'api\ProfileController@edit');
+Route::post('/v1/profile', 'API\ProfileController@show');
+Route::post('/v1/profile/history', 'API\ProfileController@challengeHistory');
+Route::post('/v1/profile/pointbalance/', 'API\ProfileController@balance');
+Route::post('/v1/profile/edit', 'API\ProfileController@edit');
 
-Route::post('/v1/messages', 'api\MessagesController@index');
-Route::post('/v1/messages/show', 'api\MessagesController@show');
-Route::post('/v1/messages/delete', 'api\MessagesController@delete');
+Route::post('/v1/messages', 'API\MessagesController@index');
+Route::post('/v1/messages/show', 'API\MessagesController@show');
+Route::post('/v1/messages/delete', 'API\MessagesController@delete');
 
-Route::post('/v1/rewards', 'api\RewardController@index');
-Route::post('/v1/rewards/show', 'api\RewardController@show');
-Route::post('/v1/rewards/redeem', 'api\RewardController@redeem');
+Route::post('/v1/rewards', 'API\RewardController@index');
+Route::post('/v1/rewards/show', 'API\RewardController@show');
+Route::post('/v1/rewards/redeem', 'API\RewardController@redeem');
 
-Route::post('/v1/referral/create', 'api\ReferralController@create');
+Route::post('/v1/referral/create', 'API\ReferralController@create');
