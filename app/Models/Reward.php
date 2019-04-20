5424001b00049ade9ed2386c58f4421462d43a11
Brent Garner <bgarner@gmail.com>
2019-04-19 23:19:08 -0600

    fixed it

--- app/Models/Reward.php
+++ app/Models/Reward.php
@@ -44,7 +44,7 @@ class Reward extends Model
                 'reward_type_id' => $request->reward_type_id,
                 'description' => $request->description,
                 'points' => $request->points,
-                'image' => "v" . $upload['version'] . "/" . $upload['public_id'] . "." . $upload['format'];
+                'image' => "v" . $upload['version'] . "/" . $upload['public_id'] . "." . $upload['format'],
                 'active_status' => 1
             ]);
         }
