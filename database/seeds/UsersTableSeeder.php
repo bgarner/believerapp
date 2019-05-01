ef4f27769e7796f1dc406d841ef609e292bacfee
Brent Garner <bgarner@gmail.com>
2019-04-30 20:09:26 -0600

    seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,21 +15,33 @@ class UsersTableSeeder extends Seeder
         // DB::table('brands')->delete();
         DB::table('users')->truncate();
         $faker = Faker\Factory::create();
-        
+
         //create myself
         User::create(array('name' => 'Brent Garner','first' => 'Brent','last' => 'Garner','email' => 'bgarner@gmail.com','group_id' => 1,'email_verified_at' => now(),'password' => bcrypt('ketchup9'),'point_balance' => $faker->numberBetween(0,2500),'social_accounts' => '','level' => $faker->numberBetween(1,9),'address1' => $faker->streetAddress,'address2' => '','city' => $faker->city,'province' => $faker->stateAbbr,'postal_code' => $faker->postcode,'phone1' => $faker->phoneNumber,'phone2' => $faker->phoneNumber));
+        //create megha
         User::create(array('name' => 'Megha Jyoti','first' => 'Megha','last' => 'Jyoti','email' => 'jyoti5337@gmail.com','group_id' => 1,'email_verified_at' => now(),'password' => bcrypt('ketchup9'),'point_balance' => $faker->numberBetween(0,2500),'social_accounts' => '','level' => $faker->numberBetween(1,9),'address1' => $faker->streetAddress,'address2' => '','city' => $faker->city,'province' => $faker->stateAbbr,'postal_code' => $faker->postcode,'phone1' => $faker->phoneNumber,'phone2' => $faker->phoneNumber));
+
+        //create sam admin
+        User::create(array('name' => 'Sam Hudson','first' => 'Sam','last' => 'Hudson','email' => 'sam.hudson@culturebrand.com','group_id' => 1,'email_verified_at' => now(),'password' => bcrypt('ketchup9'),'point_balance' => $faker->numberBetween(0,2500),'social_accounts' => '','level' => $faker->numberBetween(1,9),'address1' => $faker->streetAddress,'address2' => '','city' => $faker->city,'province' => $faker->stateAbbr,'postal_code' => $faker->postcode,'phone1' => $faker->phoneNumber,'phone2' => $faker->phoneNumber));
+
+        //create sam client
+        User::create(array('name' => 'Sam Hudson','first' => 'Sam','last' => 'Hudson','email' => 'sam.hudson@morrisonhomes.ca','group_id' => 2,'email_verified_at' => now(),'password' => bcrypt('ketchup9'),'point_balance' => $faker->numberBetween(0,2500),'social_accounts' => '','level' => $faker->numberBetween(1,9),'address1' => $faker->streetAddress,'address2' => '','city' => $faker->city,'province' => $faker->stateAbbr,'postal_code' => $faker->postcode,'phone1' => $faker->phoneNumber,'phone2' => $faker->phoneNumber));
+
+        //create sam user
+        User::create(array('name' => 'Sam Hudson','first' => 'Sam','last' => 'Hudson','email' => 'sam.hudson@gmail.com','group_id' => 3,'email_verified_at' => now(),'password' => bcrypt('ketchup9'),'point_balance' => $faker->numberBetween(0,2500),'social_accounts' => '','level' => $faker->numberBetween(1,9),'address1' => $faker->streetAddress,'address2' => '','city' => $faker->city,'province' => $faker->stateAbbr,'postal_code' => $faker->postcode,'phone1' => $faker->phoneNumber,'phone2' => $faker->phoneNumber));
+
+
 
         for($i = 0; $i < 500; $i++) {
             $first = $faker->firstName;
             $last = $faker->lastName;
-            
+
 			$user = User::create(
                 array(
-                    'name' => $first . " " . $last, 
+                    'name' => $first . " " . $last,
                     'first' => $first,
                     'last' => $last,
-                    'email' => $first . "." . $last . "@gmail.com", 
+                    'email' => $first . "." . $last . "@gmail.com",
                     'group_id' => $faker->numberBetween(1,3),
                     'email_verified_at' => now(),
                     'password' => bcrypt('ketchup9'),
