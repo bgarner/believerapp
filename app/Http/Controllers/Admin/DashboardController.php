6cafe2c6b0c626b14a3d2f05b92d642ed2ca432d
Brent Garner <bgarner@gmail.com>
2019-05-20 21:16:08 -0600

    clean up

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -30,22 +30,20 @@ class DashboardController extends Controller
         $challenges_completed = [];
         $redemptions_claimed = [];
         for ($i = 10; $i >= 1; $i--) {
-
+            //days
+            array_push($days,Carbon::now()->subDays($i)->format('F j'));
             //new users
             $user_count = User::whereBetween('created_at', [Carbon::now()->subDays($i), Carbon::now()->subDays($i-1)])->where('group_id', 3)->count();
+            array_push($new_users, $user_count);
             //new_clients
             $client_count = Client::whereBetween('created_at', [Carbon::now()->subDays($i), Carbon::now()->subDays($i-1)])->count();
+            array_push($new_clients, $client_count);
             //challenges done
             $challenge_count = ChallengeCompletion::whereBetween('created_at', [Carbon::now()->subDays($i), Carbon::now()->subDays($i-1)])->count();
+            array_push($challenges_completed, $challenge_count);
             //redemptions
             $redemptions = Redemption::whereBetween('created_at', [Carbon::now()->subDays($i), Carbon::now()->subDays($i-1)])->count();
-
-            array_push($days,Carbon::now()->subDays($i)->format('F j')); //days array
-            array_push($new_users, $user_count); //new users
-            array_push($new_clients, $client_count);
-            array_push($challenges_completed, $challenge_count);
             array_push($redemptions_claimed, $redemptions);
-
         }
 
         return view('admin.dashboard.index')
