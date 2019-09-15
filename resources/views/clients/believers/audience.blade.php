05a688938e0292fc0a6aee5e2f93449755404542
Brent Garner <bgarner@gmail.com>
2019-09-14 19:47:32 -0600

    hiding the audience stuff

--- resources/views/clients/believers/audience.blade.php
+++ resources/views/clients/believers/audience.blade.php
@@ -12,9 +12,9 @@
 <li class="nav-item">
     <a class="nav-link" href="/client/believers/invite" role=""><i class="fa fa-envelope-o" aria-hidden="true"></i> Invite</a>
 </li>
-<li class="nav-item">
+{{-- <li class="nav-item">
     <a class="nav-link" href="/client/believers/audiences" role=""><i class="fa fa-users" aria-hidden="true"></i> Audiences</a>
-</li>
+</li> --}}
 @endsection
 
 @section('content')
