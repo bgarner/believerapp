4450e23058e4da1e4db812769aba65790c081a8a
Brent Garner <bgarner@gmail.com>
2019-09-14 18:52:44 -0600

    update to redemption mailable

--- resources/views/email/redemption.blade.php
+++ resources/views/email/redemption.blade.php
@@ -2,18 +2,18 @@
     <p>New reward redemption!</p>
 
     <p>
-        {{ reward_name }} <br />
-        {{ reward_desc }}
+        {{ $reward_name }} <br />
+        {{ $reward_desc }}
     </p>
     <p>
-        {{ name }} <br />
-        {{ email }} <br />
-        {{ address }} <br />
-        {{ address2 }} <br />
-        {{ city }} <br />
-        {{ province }} <br />
-        {{ postal_code }} <br />
-        {{ phone1 }} <br />
-        {{ phone2 }}
+        {{ $name }} <br />
+        {{ $email }} <br />
+        {{ $address }} <br />
+        {{ $address2 }} <br />
+        {{ $city }} <br />
+        {{ $province }} <br />
+        {{ $postal_code }} <br />
+        {{ $phone1 }} <br />
+        {{ $phone2 }}
     </p>
 </div>
