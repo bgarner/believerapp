f073ebba0bcc7890d9d589ee2dc73ddb436355c2
Brent Garner <bgarner@gmail.com>
2019-09-14 23:11:04 -0600

    add favs property to fav missions

--- app/Models/Fav.php
+++ app/Models/Fav.php
@@ -18,11 +18,15 @@ class Fav extends Model
 
     public static function getFavsByUserId($user_id)
     {
-        return  Fav::join('challenges', 'favs.mission_id', '=', 'challenges.id')
+        $favs = Fav::join('challenges', 'favs.mission_id', '=', 'challenges.id')
                     ->join('brands', 'brands.id', '=', 'challenges.brand_id')
                     ->where('user_id', $user_id)
                     ->where('end', '>', Carbon::now())
                     ->select('challenges.*', 'brands.name as brand_name', 'brands.logo as client_logo')
                     ->get();
+        foreach($favs as $f){
+            $f->is_fav = 1;
+        }
+        return $favs;
     }
 }
