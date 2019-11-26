94800a44a207a8efc1d826558187ada1e45d2d3f
Brent Garner <bgarner@gmail.com>
2019-11-25 22:06:42 -0700

    endif in wrong spot

--- resources/views/admin/redemptions/index.blade.php
+++ resources/views/admin/redemptions/index.blade.php
@@ -33,39 +33,41 @@
                     No redemptions yet....
                     </td>
                 </tr>
+                @else
+
+                    @foreach($redemptions as $r)
+                    <tr id="redemption{{ $r->id }}">
+                        <td><strong>{{ $r->user->first }} {{ $r->user->last}}</strong>
+                            <p style="font-weight: normal; line-height: 1.2">
+                                {{$r->user->address1}}<br />
+                                @if($r->user->address2)
+                                {{$r->user->address2}}<br />
+                                @endif
+                                {{$r->user->city}}, {{$r->user->province}}<br />
+                                {{$r->user->postal_code}}<br />
+                                {{$r->user->email}}<br />
+                            </p>
+                        </td>
+                        <td>{{ $r->reward->title }}</td>
+                        <td>{{ $r->created_at }}</td>
+                        <td>{{ $r->completed }}</td>
+                        {{-- <td>{{ $client->total_believers }}</td>
+                        <td>{{ $client->challenge_completions }}</td>
+                        <td>{{ $client->total_points }}</td> --}}
+                        <td>
+                            <h5>
+                                @if($r->completed)
+                                <a href="#" class="redeemed markAsRedeemed" data-item-id="{{ $r->id }}"><i class="fa fa-check"></i></a>
+                                @else
+                                <a href="#" class="notRedeemed markAsRedeemed" data-item-id="{{ $r->id }}"><i class="fa fa-check"></i></a>
+                                @endif
+                            </h5>
+                        </td>
+                    </tr>
+
+                    @endforeach
+                    
                 @endif
-
-                @foreach($redemptions as $r)
-                <tr id="redemption{{ $r->id }}">
-                    <td><strong>{{ $r->user->first }} {{ $r->user->last}}</strong>
-                        <p style="font-weight: normal; line-height: 1.2">
-                            {{$r->user->address1}}<br />
-                            @if($r->user->address2)
-                            {{$r->user->address2}}<br />
-                            @endif
-                            {{$r->user->city}}, {{$r->user->province}}<br />
-                            {{$r->user->postal_code}}<br />
-                            {{$r->user->email}}<br />
-                        </p>
-                    </td>
-                    <td>{{ $r->reward->title }}</td>
-                    <td>{{ $r->created_at }}</td>
-                    <td>{{ $r->completed }}</td>
-                    {{-- <td>{{ $client->total_believers }}</td>
-                    <td>{{ $client->challenge_completions }}</td>
-                    <td>{{ $client->total_points }}</td> --}}
-                    <td>
-                        <h5>
-                            @if($r->completed)
-                            <a href="#" class="redeemed markAsRedeemed" data-item-id="{{ $r->id }}"><i class="fa fa-check"></i></a>
-                            @else
-                            <a href="#" class="notRedeemed markAsRedeemed" data-item-id="{{ $r->id }}"><i class="fa fa-check"></i></a>
-                            @endif
-                        </h5>
-                    </td>
-                </tr>
-
-                @endforeach
                 </tbody>
                 </table>
             </div>
